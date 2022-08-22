5901e991225a39753dbf0308d063adabca308bf4
Bernd Porr <mail@berndporr.me.uk>
2022-08-22 12:07:00 +0200

    OpenXR compiles

--- app/.cxx/Debug/v1o56323/x86/cmake_install.cmake
+++ app/.cxx/Debug/v1o56323/x86/cmake_install.cmake
@@ -42,6 +42,11 @@ if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "/home/bp1/Android/Sdk/ndk/21.4.7075529/toolchains/llvm/prebuilt/linux-x86_64/bin/i686-linux-android-objdump")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/tmp/cmake_install.cmake")
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
