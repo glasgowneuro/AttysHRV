66e1a2b8f6681363798b634f2eb5629324e894a6
Bernd Porr <mail@berndporr.me.uk>
2023-06-05 10:46:00 +0100

    Added start date

--- plot/plot_hr.py
+++ plot/plot_hr.py
@@ -11,6 +11,8 @@ hrs = []
 
 data = np.loadtxt("attyshrv_heartrate.tsv")
 
+startdatetime = datetime.datetime.fromtimestamp(float(data[0,0])/1000.)
+
 for i in data:
     customdate = datetime.datetime.fromtimestamp(float(i[0])/1000.)
     ts.append(customdate)
@@ -20,8 +22,8 @@ t = "Heartrate"
 plt.figure(t)
 plt.title(t)
 plt.plot(ts,hrs)
-plt.xlabel("Date/time")
+plt.xlabel("Date/Time (starts at {})".format(startdatetime))
 plt.ylabel("HR/BPM")
-plt.ylim([0,200])
+plt.ylim([0,130])
 
 plt.show()
